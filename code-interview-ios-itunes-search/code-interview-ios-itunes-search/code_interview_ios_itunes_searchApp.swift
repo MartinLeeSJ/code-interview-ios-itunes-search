dbb39e75d3814b45d4704dcb432387001d867fe7
MartinLeeSJ <tjr2922@gmail.com>
2023-08-23 18:15:35 +0900

    [Add] Add Project

---
+++ code-interview-ios-itunes-search/code-interview-ios-itunes-search/code_interview_ios_itunes_searchApp.swift
@@ -0,0 +1,17 @@
+//
+//  code_interview_ios_itunes_searchApp.swift
+//  code-interview-ios-itunes-search
+//
+//  Created by Martin on 2023/08/23.
+//
+
+import SwiftUI
+
+@main
+struct code_interview_ios_itunes_searchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
